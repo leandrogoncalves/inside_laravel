1f447168da4ff3323aa4cd868d3ad1d5f166c91c
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-14 17:46:27 -0200

    Criado os modelos de Laboratorio e Formulario

---
+++ app/Models/Laboratorio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Inside\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Laboratorio extends Model
+{
+    protected $table = 'pespeslab';
+    protected $connection = 'psy_transp';
+}
