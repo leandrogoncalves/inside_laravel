cc9e964f592630f276b038a9125534b95a99b5ef
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-15 15:04:29 -0200

    Agora a HomeController está retornando quando há erro.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,24 @@
 namespace Inside\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inside\Services\HomeService;
 
 class HomeController extends Controller
 {
+    private $service;
+
+    public function __construct(HomeService $service)
+    {
+        $this->service = $service;
+    }
+
     public function index(Request $request)
     {
-        $data = 'olá mundo';
-        //return response()->json($data);
-        return view('home', compact('data'));
+        try {
+            $data = $this->service->getData($request);
+            return response()->json($data);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), $e->getCode());
+        }
     }
 }
