6a993f1a513260fdbeab7ec44a29aafb0082f530
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-15 20:37:15 -0200

    Mudei a forma de como pegar o Total de Exames.

--- app/Services/SolicitacoesExames/TotalExames.php
+++ app/Services/SolicitacoesExames/TotalExames.php
@@ -21,6 +21,19 @@ class TotalExames
 
     public function getTotalExamesSolicitados(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
     {
+        if ($user->isUserPsy()) {
+            return $this->getTotalExamesSolicitadosPsy($dataInicio, $dataFim, $user);
+        }
+
+        if ($user->isUserPardini()) {
+            return $this->getTotalExamesSolicitadosPardini($dataInicio, $dataFim, $user);
+        }
+
+        throw new Exception("Usuário de perfil inválido, não é nem Pardini e nem Psy.", 400);
+    }
+
+    private function getTotalExamesSolicitadosPsy(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
+    {
         $idExecutivoPsy = $this->executivos->getIdExecutivo($user);
         $dataInicio = $this->getDateFormatted($dataInicio);
         $dataFim = $this->getDateFormatted($dataFim);
@@ -42,6 +55,33 @@ class TotalExames
         ->orderBy("OprFrmOrigem")
         ->all(["OprFrmOrigem", DB::raw("count(1) as total")]);
     }
+
+    private function getTotalExamesSolicitadosPardini(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
+    {
+        $idExecutivo = $this->executivos->getIdExecutivo($user);
+        $dataInicio = $this->getDateFormatted($dataInicio);
+        $dataFim = $this->getDateFormatted($dataFim);
+
+        return $this->formularioRepository
+        ->scopeQuery(function ($query) use ($dataInicio, $dataFim) {
+            return $query
+            ->where("OprFrmDtHrIncl", ">=", $dataInicio)
+            ->where("OprFrmDtHrIncl", "<=", $dataFim)
+            ->where("OprFrmStatus", "<>", "C")
+            ->where("OprFrmOrigem", "<>", "CAG")
+            ->where("OprFrmOrigem", "<>", "LAB");
+        })
+        ->with(["laboratorio"])
+        ->whereHas("laboratorio", function ($query) use ($idExecutivo) {
+            $query
+            ->where("PesPesRedeAuto", "=", "1")
+            ->whereIn('id_laboratorio', $idExecutivo);
+        })
+        ->groupBy("OprFrmOrigem")
+        ->orderBy("OprFrmOrigem")
+        ->all(["OprFrmOrigem", DB::raw("count(1) as total")]);
+    }
+
 
     private function getDateFormatted(Carbon $date)
     {
