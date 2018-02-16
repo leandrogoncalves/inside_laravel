626948358f26fb867fa10074b936361459302bc9
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-16 18:56:58 -0200

    Removido os menus de dentro Controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,12 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    protected $view = [];
-
-    public function __construct()
-    {
-        $this->view['title'] = 'Inside';
-        $this->view['menu'] = 'home';
-    }
 }
