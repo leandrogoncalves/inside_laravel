d114541c85d62884b5db193ef0096fb3501ba065
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-16 09:20:19 -0200

    Dividi os totais de exames por origem.

--- app/Domain/SolicitacoesExames/TotalExames/TotalExames.php
+++ app/Domain/SolicitacoesExames/TotalExames/TotalExames.php
@@ -2,92 +2,49 @@
 
 namespace Inside\Domain\SolicitacoesExames\TotalExames;
 
-use Inside\Repositories\Contracts\LaboratorioRepository;
-use Inside\Repositories\Contracts\FormularioRepository;
-
 use Inside\Domain\SolicitacoesExames\UsuarioLogado;
 use Inside\Domain\SolicitacoesExames\Executivos\Executivos;
 
+use Inside\Domain\SolicitacoesExames\TotalExames\OrigemBasica;
+use Inside\Domain\SolicitacoesExames\TotalExames\OrigemLaboratorio;
+use Inside\Domain\SolicitacoesExames\TotalExames\OrigemLaboratorioCltCnh;
+
 use Carbon\Carbon;
-use \DB;
 
 class TotalExames
 {
-    private $formularioRepository;
     private $executivos;
+    private $origemBasica;
 
-    public function __construct(Executivos $executivos, FormularioRepository $formularioRepository)
+
+    public function __construct(Executivos $executivos, OrigemBasica $origemBasica, OrigemLaboratorio $origemLaboratorio, OrigemLaboratorioCltCnh $origemLaboratorioCltCnh)
     {
-        $this->formularioRepository = $formularioRepository;
         $this->executivos = $executivos;
+        $this->origemBasica = $origemBasica;
+        $this->origemLaboratorio = $origemLaboratorio;
+        $this->origemLaboratorioCltCnh = $origemLaboratorioCltCnh;
     }
 
     public function getTotalExamesSolicitados(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
     {
+        $idExecutivo = $this->executivos->getIdExecutivo($user);
+
         if ($user->isUserPsy()) {
-            return $this->getTotalExamesSolicitadosPsy($dataInicio, $dataFim, $user);
+            return collect([
+                $this->origemBasica->getTotalExamesSolicitadosPsy($dataInicio, $dataFim, $idExecutivo),
+                $this->origemLaboratorio->getTotalExamesSolicitadosPsy($dataInicio, $dataFim, $idExecutivo),
+                $this->origemLaboratorioCltCnh->getTotalExamesSolicitadosPsy($dataInicio, $dataFim, $idExecutivo),
+            ]);
         }
 
         if ($user->isUserPardini()) {
-            return $this->getTotalExamesSolicitadosPardini($dataInicio, $dataFim, $user);
+            return collect([
+                $this->origemBasica->getTotalExamesSolicitadosPardini($dataInicio, $dataFim, $idExecutivo),
+                $this->origemLaboratorio->getTotalExamesSolicitadosPardini($dataInicio, $dataFim, $idExecutivo),
+                $this->origemLaboratorioCltCnh->getTotalExamesSolicitadosPardini($dataInicio, $dataFim, $idExecutivo),
+            ]);
         }
 
         throw new Exception("Usuário de perfil inválido, não é nem Pardini e nem Psy.", 400);
-    }
-
-    private function getTotalExamesSolicitadosPsy(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
-    {
-        $idExecutivoPsy = $this->executivos->getIdExecutivo($user);
-        $dataInicio = $this->getDateFormatted($dataInicio);
-        $dataFim = $this->getDateFormatted($dataFim);
-
-        return $this->formularioRepository
-        ->scopeQuery(function ($query) use ($dataInicio, $dataFim) {
-            return $query
-            ->where("OprFrmDtHrIncl", ">=", $dataInicio)
-            ->where("OprFrmDtHrIncl", "<=", $dataFim)
-            ->where("OprFrmStatus", "<>", "C")
-            ->where("OprFrmOrigem", "<>", "CAG")
-            ->where("OprFrmOrigem", "<>", "LAB");
-        })
-        ->with(["laboratorio"])
-        ->whereHas("laboratorio", function ($query) use ($idExecutivoPsy) {
-            $query->whereIn('id_executivo_psy', $idExecutivoPsy);
-        })
-        ->groupBy("OprFrmOrigem")
-        ->orderBy("OprFrmOrigem")
-        ->all(["OprFrmOrigem", DB::raw("count(1) as total")]);
-    }
-
-    private function getTotalExamesSolicitadosPardini(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
-    {
-        $idExecutivo = $this->executivos->getIdExecutivo($user);
-        $dataInicio = $this->getDateFormatted($dataInicio);
-        $dataFim = $this->getDateFormatted($dataFim);
-
-        return $this->formularioRepository
-        ->scopeQuery(function ($query) use ($dataInicio, $dataFim) {
-            return $query
-            ->where("OprFrmDtHrIncl", ">=", $dataInicio)
-            ->where("OprFrmDtHrIncl", "<=", $dataFim)
-            ->where("OprFrmStatus", "<>", "C")
-            ->where("OprFrmOrigem", "<>", "CAG")
-            ->where("OprFrmOrigem", "<>", "LAB");
-        })
-        ->with(["laboratorio"])
-        ->whereHas("laboratorio", function ($query) use ($idExecutivo) {
-            $query
-            ->where("PesPesRedeAuto", "=", "1")
-            ->whereIn('id_laboratorio', $idExecutivo);
-        })
-        ->groupBy("OprFrmOrigem")
-        ->orderBy("OprFrmOrigem")
-        ->all(["OprFrmOrigem", DB::raw("count(1) as total")]);
-    }
-
-
-    private function getDateFormatted(Carbon $date)
-    {
-        return $date->toDateTimeString();
     }
 }
