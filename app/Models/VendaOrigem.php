e230c87fd402875b66d2efd59fb9d2cbf03174d0
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-17 10:19:36 -0200

    Criado o Model Venda Origem.

---
+++ app/Models/VendaOrigem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Inside\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VendaOrigem extends Model
+{
+    protected $table = 'dw_vendas_origem';
+    protected $connection = 'inteligencia';
+}
