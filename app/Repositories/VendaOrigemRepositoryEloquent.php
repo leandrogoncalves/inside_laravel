8f3005a67be4f352c52b785c0c951d51efc99630
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-17 10:20:35 -0200

    Venda Origem Repository criado.

---
+++ app/Repositories/VendaOrigemRepositoryEloquent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Inside\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Inside\Repositories\Contracts\VendaOrigemRepository;
+use Inside\Models\VendaOrigem;
+use Inside\Validators\VendaOrigemValidator;
+
+/**
+ * Class VendaOrigemRepositoryEloquent.
+ *
+ * @package namespace Inside\Repositories;
+ */
+class VendaOrigemRepositoryEloquent extends BaseRepository implements VendaOrigemRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return VendaOrigem::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+    
+}
