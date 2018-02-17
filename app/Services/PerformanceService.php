c0d9c624ec5cc9c9e2756634324cfa27165279b9
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-17 18:56:05 -0200

    Criado o performance service.

---
+++ app/Services/PerformanceService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Inside\Services;
+
+use Illuminate\Http\Request;
+use Inside\Services\UsuarioLogadoService;
+
+class PerformanceService
+{
+    private $usuarioLogadoService;
+
+    public function __construct(UsuarioLogadoService $usuarioLogadoService)
+    {
+        $this->usuarioLogadoService = $usuarioLogadoService;
+    }
+
+    public function getData(Request $request)
+    {
+        return 'ola mundo';
+    }
+
+}
