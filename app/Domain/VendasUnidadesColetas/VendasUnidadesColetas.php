9fd970fce45ef5b1b28ac0f5164bf996ee70540f
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-18 14:22:18 -0300

    O service de performance chama vendas unidades coletas.

--- app/Domain/VendasUnidadesColetas/VendasUnidadesColetas.php
+++ app/Domain/VendasUnidadesColetas/VendasUnidadesColetas.php
@@ -2,6 +2,8 @@
 
 namespace Inside\Domain\VendasUnidadesColetas;
 
+use Inside\Domain\VendasUnidadesColetas\ComVenda\UnidadesColetasComVenda;
+
 use Inside\Domain\UsuarioLogado;
 use Inside\Domain\Executivos\Executivos;
 use Carbon\Carbon;
@@ -9,53 +11,21 @@ use \DB;
 
 class VendasUnidadesColetas
 {
-    private $user;
     private $executivos;
-    private $vendaOrigemRepository;
+    private $unidadesColetasComVenda;
 
-    public function __construct(Executivos $executivos, VendaOrigemRepository $vendaOrigemRepository)
+    public function __construct(Executivos $executivos, UnidadesColetasComVenda $unidadesColetasComVenda)
     {
         $this->executivos = $executivos;
-        $this->vendaOrigemRepository = $vendaOrigemRepository;
+        $this->unidadesColetasComVenda = $unidadesColetasComVenda;
     }
 
     public function get(UsuarioLogado $user)
     {
-        $this->setUser($user);
         $idExecutivo = $this->executivos->getIdExecutivo($user);
-    }
+        $dataInicio = Carbon::now()->copy()->subMonth(1)->hour(0)->minute(0)->second(0);
+        $dataFim = Carbon::now()->copy()->hour(23)->minute(59)->second(59);
 
-    private function getTotalExamesAgrupadosPardini($dataInicio, $dataFim, array $idExecutivo)
-    {
-        return $this->vendaOrigemRepository
-        ->scopeQuery(function ($query) use ($dataInicio, $dataFim) {
-            return $query
-            ->where("data_inclusao", ">=", $dataInicio)
-            ->where("data_inclusao", "<=", $dataFim);
-        })
-        ->with(["laboratorio"])
-        ->whereHas("laboratorio", function ($query) use ($idExecutivo) {
-            $query->whereIn('id_laboratorio', $idExecutivo);
-        })
-        ->groupBy("data_inclusao")
-        ->orderBy("data_inclusao")
-        ->all(["data_inclusao", DB::raw("IFNULL(SUM(quantidade), 0) AS quantidade")]);
-    }
-
-    private function getTotalExamesAgrupadosPsyNotAdmin($dataInicio, $dataFim, array $idExecutivo)
-    {
-        return $this->vendaOrigemRepository
-        ->scopeQuery(function ($query) use ($dataInicio, $dataFim) {
-            return $query
-            ->where("data_inclusao", ">=", $dataInicio)
-            ->where("data_inclusao", "<=", $dataFim);
-        })
-        ->with(["laboratorio"])
-        ->whereHas("laboratorio", function ($query) use ($idExecutivo) {
-            $query->whereIn('id_executivo_psy', $idExecutivo);
-        })
-        ->groupBy("data_inclusao")
-        ->orderBy("data_inclusao")
-        ->all(["data_inclusao", DB::raw("IFNULL(SUM(quantidade), 0) AS quantidade")]);
+        dd($this->unidadesColetasComVenda->getUnidadesColetasPsy($dataInicio, $dataFim, $idExecutivo));
     }
 }
--- app/Services/PerformanceService.php
+++ app/Services/PerformanceService.php
@@ -4,18 +4,23 @@ namespace Inside\Services;
 
 use Illuminate\Http\Request;
 use Inside\Services\UsuarioLogadoService;
+use Inside\Domain\VendasUnidadesColetas\VendasUnidadesColetas;
 
 class PerformanceService
 {
     private $usuarioLogadoService;
+    private $vendasUnidadesColetas;
 
-    public function __construct(UsuarioLogadoService $usuarioLogadoService)
+    public function __construct(UsuarioLogadoService $usuarioLogadoService, VendasUnidadesColetas $vendasUnidadesColetas)
     {
         $this->usuarioLogadoService = $usuarioLogadoService;
+        $this->vendasUnidadesColetas = $vendasUnidadesColetas;
     }
 
     public function getData(Request $request)
     {
+        $user = $this->usuarioLogadoService->getUsuarioLogadoData($request);
+        $this->vendasUnidadesColetas->get($user);
         return 'ola mundo';
     }
 }
