7ac66c82651b8cc75fbb86c878a3398903ae7a6f
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-18 15:30:27 -0300

    Agora fica sobre responsabilidade do performance setar a data inicio e a data fim.

--- app/Services/PerformanceService.php
+++ app/Services/PerformanceService.php
@@ -5,6 +5,7 @@ namespace Inside\Services;
 use Illuminate\Http\Request;
 use Inside\Services\UsuarioLogadoService;
 use Inside\Domain\VendasUnidadesColetas\VendasUnidadesColetas;
+use Carbon\Carbon;
 
 class PerformanceService
 {
@@ -20,7 +21,10 @@ class PerformanceService
     public function getData(Request $request)
     {
         $user = $this->usuarioLogadoService->getUsuarioLogadoData($request);
-        $this->vendasUnidadesColetas->get($user);
+        $dataInicio = $request->exists('data_inicio')? $request->only('data_inicio') : Carbon::now()->copy()->subMonth(1)->hour(0)->minute(0)->second(0);
+        $dataFim = $request->exists('data_fim')? $request->only('data_fim') : Carbon::now()->copy()->hour(23)->minute(59)->second(59);
+
+        $this->vendasUnidadesColetas->get($dataInicio, $dataFim, $user);
         return 'ola mundo';
     }
 }
