1bae61d6151ab6d14c44b46ad9ef93d0f88c99ad
Caue Prado <caue.prado@psychemedics.com.br>
2018-02-18 22:59:39 -0300

    Novo jeito de pegar os dados da performance quando a data estiver diferente do padrão.

--- app/Domain/VendasUnidadesColetas/VendasUnidadesColetas.php
+++ app/Domain/VendasUnidadesColetas/VendasUnidadesColetas.php
@@ -6,6 +6,7 @@ use Inside\Domain\VendasUnidadesColetas\ComVenda\UnidadesColetaComVendaDetail;
 use Inside\Domain\VendasUnidadesColetas\ComVenda\UnidadesColetasComVenda;
 use Inside\Domain\VendasUnidadesColetas\SemVenda\UnidadesColetasSemVenda;
 use Inside\Domain\VendasUnidadesColetas\NuncaVenderam\UnidadesColetasNuncaVenderam;
+use Inside\Domain\VendasUnidadesColetas\PerformanceLaboratorioNewDates\PerformanceLaboratorioNewDates;
 
 use Inside\Domain\UsuarioLogado;
 use Inside\Domain\Executivos\Executivos;
@@ -18,31 +19,26 @@ class VendasUnidadesColetas
     private $unidadesColetasComVenda;
     private $unidadesColetasSemVenda;
     private $unidadesColetasNuncaVenderam;
+    private $unidadesColetaComVendaDetail;
+    private $performanceLaboratorioNewDates;
 
     public function __construct(Executivos $executivos,
                                 UnidadesColetasComVenda $unidadesColetasComVenda,
                                 UnidadesColetasSemVenda $unidadesColetasSemVenda,
                                 UnidadesColetasNuncaVenderam $unidadesColetasNuncaVenderam,
-                                UnidadesColetaComVendaDetail $unidadesColetaComVendaDetail
-    )
+                                UnidadesColetaComVendaDetail $unidadesColetaComVendaDetail,
+                                PerformanceLaboratorioNewDates $performanceLaboratorioNewDates
+                                )
     {
         $this->executivos = $executivos;
         $this->unidadesColetasComVenda = $unidadesColetasComVenda;
         $this->unidadesColetasSemVenda = $unidadesColetasSemVenda;
         $this->unidadesColetasNuncaVenderam = $unidadesColetasNuncaVenderam;
         $this->unidadesColetaComVendaDetail = $unidadesColetaComVendaDetail;
+        $this->performanceLaboratorioNewDates = $performanceLaboratorioNewDates;
     }
 
     public function get(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user, bool $isDefaultDate = true)
-    {
-        if ($isDefaultDate) {
-            return $this->returnDataIfIsDefaultDate($dataInicio, $dataFim, $user);
-        }
-
-        return $this->returnDataIfIsNotDefaultDate($dataInicio, $dataFim, $user);
-    }
-
-    private function returnDataIfIsDefaultDate(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
     {
         $comVenda = $semVenda = $nuncaVenderam = $comVendaDetail = 0;
         $idExecutivo = $this->executivos->getIdExecutivo($user);
@@ -51,7 +47,12 @@ class VendasUnidadesColetas
             $comVenda = $this->unidadesColetasComVenda->getUnidadesColetasPsy($dataInicio, $dataFim, $idExecutivo);
             $semVenda = $this->unidadesColetasSemVenda->getUnidadesColetasPsy($dataInicio, $dataFim, $idExecutivo);
             $nuncaVenderam = $this->unidadesColetasNuncaVenderam->getUnidadesColetasPsy($dataInicio, $dataFim, $idExecutivo);
-            $comVendaDetail = $this->unidadesColetaComVendaDetail->getUnidadesColetaPsyDetail($dataInicio, $dataFim, $idExecutivo);
+
+            if ($isDefaultDate) {
+                $comVendaDetail = $this->unidadesColetaComVendaDetail->getUnidadesColetaPsyDetail($idExecutivo);
+            } else {
+                $comVendaDetail = $this->unidadesColetaPsyNotDefaultDate($dataInicio, $dataFim, $idExecutivo);
+            }
 
             return collect([
                 'unidadesColetasComVenda' => $comVenda,
@@ -67,6 +68,12 @@ class VendasUnidadesColetas
             $nuncaVenderam = $this->unidadesColetasNuncaVenderam->getUnidadesColetasPardini($dataInicio, $dataFim, $idExecutivo);
             $comVendaDetail = $this->unidadesColetaComVendaDetail->getUnidadesColetaPardiniDetail($dataInicio, $dataFim, $idExecutivo);
 
+            if ($isDefaultDate) {
+                $comVendaDetail = $this->unidadesColetaComVendaDetail->getUnidadesColetaPardiniDetail($idExecutivo);
+            } else {
+                $comVendaDetail = $this->unidadesColetaPardiniNotDefaultDate($dataInicio, $dataFim, $idExecutivo);
+            }
+
             return collect([
                 'unidadesColetasComVenda' => $comVenda,
                 'unidadesColetasSemVenda' => $semVenda,
@@ -78,8 +85,13 @@ class VendasUnidadesColetas
         throw new \Exception("Erro, perfil de acesso desconhecido", 400);
     }
 
-    private function returnDataIfIsNotDefaultDate(Carbon $dataInicio, Carbon $dataFim, UsuarioLogado $user)
+    private function unidadesColetaPsyNotDefaultDate(Carbon $dataInicio, Carbon $dataFim, array $idExecutivo)
     {
+        return $this->performanceLaboratorioNewDates->getPsy($dataInicio, $dataFim, $idExecutivo);
+    }
 
+    private function unidadesColetaPardiniNotDefaultDate(Carbon $dataInicio, Carbon $dataFim, array $idExecutivo)
+    {
+        return $this->performanceLaboratorioNewDates->getPardini($dataInicio, $dataFim, $idExecutivo);
     }
 }
