8a663a8669c4b292fe15b0cce9940bd78d875f8c
Leandro Gonçalves da Silva <contato.leandrogoncalves@gmail.com>
2018-02-22 16:00:42 -0300

    inclusao do tablesorter via npm

---
+++ public/js/inside.js
@@ -0,0 +1,89 @@
+var global = {};
+
+var inside = inside || {};
+
+inside.home = {
+
+    init : function () {
+        this.chart_home();
+    },
+
+    CallToChart:  function (dias, quantidade) {
+
+        var canvas = document.getElementById("meuCanvas");
+        var myChart = new Chart(canvas, {
+            type: 'bar',
+            data:{
+                labels: dias,
+                datasets:[{
+                    label:'coletas',
+                    data:quantidade,
+                    backgroundColor:'rgba(14,72, 100, 0.8)'
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Acumulado nos Ãºltimos 6 meses'
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        ticks: {
+                            beginAtZero: true,
+                        }
+                    }]
+                },
+            }
+        })
+    },
+
+    convertDate: function(date) {
+        var grafico = date;
+        var dias = [];
+        var quantidade = [];
+        for(var i=0; i <  Object.keys(grafico).length; i++){
+            dias.push(grafico[i].data_inclusao);
+            quantidade.push(grafico[i].quantidade);
+        }
+        this.CallToChart(dias, quantidade );
+    },
+
+    loadData: function (link, _callBackBefore, _callBackAfter) {
+        $.ajax({
+            url:link,
+            context: document.body,
+            beforeSend: _callBackBefore,
+            complete: _callBackAfter
+        }).done(function(data) {
+            global.home.convertDate(data);
+        });;
+    },
+
+    chart_home : function () {
+        var url = '/total-exames-acumulado/'+$('#id_executivo').text()+'/'+$('#perfil_acesso').text();
+        this.loadData(url, function () {}, function () {});
+    }
+}
+var global = global || {};
+
+global.performance_lab = {
+
+    init: function () {
+        this.laod_tablesorter();
+    },
+
+    laod_tablesorter: function () {
+        $(document).ready(function(){
+            $(".table-sorter").tablesorter();
+        });
+    }
+}
